54c6dedc401c9eff8d58a268438c9dbef1b565ae
SimuTheProgrammer <82083847+SimuTheProgrammer@users.noreply.github.com>
2021-04-08 06:52:48 +0200

    Update calibrationPipe.py

--- calibrationPipe.py
+++ calibrationPipe.py
@@ -1,5 +1,6 @@
 from astropy.io import fits
 import numpy as np
+import statistics
 
 class CalibrationPipe():
     def __init__(self, image_path, dark, bias, flat):
@@ -43,21 +44,44 @@ class CalibrationPipe():
         imagedata = np.subtract(imagedata, darkdata)
         return imagedata
     
-    def stack_images(self, images):
-        l = []
-        for i in images:
-            data = i[0].data
-            l.append(data)
+    def stack_images(self, images, calculation):
+        if calculation == "median":
+            master_image = images[0]
+            l = []
+            for i in images:
+                data = i[0].data
+                l.append(data)
+                
+            for y in np.array(master_image[0].data).tolist():
+                for x in y:
+                    m = []
+                    for i in l:
+                        m.append(i[y][x])
+                    statistics.median(m)
+                
             
-        for i in range(len(images) - 1):
-            l[i] = np.add(l[i], l[i + 1])
-        
-        stacked_images = np.true_divide(l[i], len(images))
-        print(stacked_images)
+            """for i in l:
+                xList = []
+                for j in range(len(i)):
+                    yList = []
+                    for k in range(len(i[j])):
+                        yList.append(i[j][k])
+                    xList.append(yList)"""
+                
+                    
+                    
             
+        elif calculation == "average":
+            l = []
+            for i in images:
+                data = i[0].data
+                l.append(data)
+            
+            for i in range(len(images) - 1):
+                l[i] = np.add(l[i], l[i + 1])
         
-        
-              
+            stacked_images = np.true_divide(l[i], len(images))
+            print(stacked_images)      
         
 print("start")
 
@@ -70,6 +94,6 @@ calibPip = CalibrationPipe(a, b, c, d)
 calibPip.calibrate()
 
 imgs = [fits.open(a), b]
-calibPip.stack_images(imgs)
+calibPip.stack_images(imgs, "median")
 
 print("stop")
