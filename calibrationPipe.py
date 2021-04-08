309c90ab8db74b410a3b79f759182837fbcb31a0
SimuTheProgrammer <82083847+SimuTheProgrammer@users.noreply.github.com>
2021-04-08 11:45:51 +0200

    Update calibrationPipe.py

--- calibrationPipe.py
+++ calibrationPipe.py
@@ -8,7 +8,8 @@ class CalibrationPipe():
         self.dark = dark
         self.bias = bias
         self.flat = flat
-    
+        self.image.info()
+        
     def calibrate(self):
         imagedata = self.image[0].data
         darkdata = self.dark[0].data
@@ -45,32 +46,25 @@ class CalibrationPipe():
         return imagedata
     
     def stack_images(self, images, calculation):
+        master_image = images[0]
+        master_image_data = master_image[0].data
+        
         if calculation == "median":
-            master_image = images[0]
             l = []
             for i in images:
-                data = i[0].data
-                l.append(data)
+                l.append(i[0].data.astype("uint"))
                 
-            for y in np.array(master_image[0].data).tolist():
-                for x in y:
+            for y in range(4095):
+                for x in range(4095):
                     m = []
                     for i in l:
-                        m.append(i[y][x])
-                    statistics.median(m)
-                
-            
-            """for i in l:
-                xList = []
-                for j in range(len(i)):
-                    yList = []
-                    for k in range(len(i[j])):
-                        yList.append(i[j][k])
-                    xList.append(yList)"""
-                
-                    
-                    
-            
+                        m.append(i[x][y])
+                    master_image_data[x][y] = statistics.median(m)
+                    # print("iteration")
+            print(master_image_data)
+            master_image[0].data = master_image_data
+            master_image.writeto("deltafile.fts")
+                       
         elif calculation == "average":
             l = []
             for i in images:
@@ -81,6 +75,8 @@ class CalibrationPipe():
                 l[i] = np.add(l[i], l[i + 1])
         
             stacked_images = np.true_divide(l[i], len(images))
+            master_image[0].data = stacked_images
+            master_image.writeto("gammafile.fts")
             print(stacked_images)      
         
 print("start")
@@ -89,11 +85,13 @@ a = '2021-04-02T19-10-26_M1_Clear_280s_Simon-H.fts'
 b = fits.open('HAT-P-10-001dark.fit')
 c = fits.open('Bias-001.fit')
 d = fits.open('HAT-P-10-001light.fit')
+e = fits.open("Reg_2021-04-03T19-27-40_M51_Red_200s_Simon-H.fit")
 
 calibPip = CalibrationPipe(a, b, c, d)
 calibPip.calibrate()
 
-imgs = [fits.open(a), b]
+imgs = [fits.open(a), e]
 calibPip.stack_images(imgs, "median")
+calibPip.stack_images(imgs, "average")
 
 print("stop")
