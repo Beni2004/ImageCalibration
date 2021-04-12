11b5d8e050f9f7e0e82f6e4844d497ac5b84618a
SimuTheProgrammer <82083847+SimuTheProgrammer@users.noreply.github.com>
2021-04-12 09:17:58 +0200

    Update calibrationPipe.py

--- calibrationPipe.py
+++ calibrationPipe.py
@@ -2,56 +2,108 @@ from astropy.io import fits
 import numpy as np
 import statistics
 import time
-from multiprocessing import Process
-import queue
+import multiprocessing as mp
+import os
+# from multiprocessing import Process
+from queue import Queue
 # import matplotlib.pyplot as plt
 
 t = time.time()
 
 class CalibrationPipe():
-    def __init__(self, image_path, dark, bias, flat):
-        self.image = fits.open(image_path)
-        self.dark = dark
-        self.bias = bias
-        self.flat = flat
-        self.image.info()
+    def __init__(self, image_path, dark_path, bias_path, flat_path):
+        with fits.open(image_path) as image:
+            self.imagedata = image[0].data
+            self.imagehdr = image[0].header
+            
+        with fits.open(dark_path) as dark:
+            self.darkdata = dark[0].data
+            self.darkhdr = dark[0].header
         
-    def calibrate(self):
-        imagedata = self.image[0].data
-        darkdata = self.dark[0].data
-        biasdata = self.bias[0].data
-        flatdata = self.flat[0].data
+        with fits.open(bias_path) as bias:
+            self.biasdata = bias[0].data
+            self.biashdr = bias[0].header
         
-        #imagedata = self.dark_frame(imagedata, darkdata, biasdata)
+        with fits.open(flat_path) as flat:
+            self.flatdata = flat[0].data
+            self.flathdr = flat[0].header
         
-        #imagedata = self.bias_frame(imagedata, biasdata)
+        self.imagepath = image_path
+        # self.image.info()
         
-        #imagedata = self.flat_frame(imagedata, flatdata)
+    def run(self):
+        print("ruuuuun!")
+        imagedata = self.imagedata
+        darkdata = self.darkdata
+        biasdata = self.biasdata
+        flatdata = self.flatdata
+        
+        imagedata = self.dark_frame(imagedata, darkdata, biasdata)
+        
+        imagedata = self.bias_frame(imagedata, biasdata)
+        
+        imagedata = self.flat_frame(imagedata, flatdata)
         
         amounty=len(imagedata)
         amountx=len(imagedata[0])
         
-        imagedata = self.run_check(imagedata, 0, 0, amountx, amounty//2)
+        # q1 = Queue()
+        # q2 = Queue()
         
-        imagedata = self.run_check(imagedata, 0, amounty//2, amountx, amounty//2)
+        print("Starting multiprocessing...")
         
-        self.image[0].data = imagedata
-        self.image.writeto("output.fts", overwrite=True)
+        processes = []
+        for i in [(imagedata, 0, 0, amountx, amounty//2), (imagedata, 0, amounty//2, amountx, amounty//2)]:
+            p = mp.Process(target = self.run_check, args = i)
+            processes.append(p)
+            print("Iteration")
+        
+        [x.start() for x in processes]
+            
+        print("All processes should have started now")
+        
+        [x.join() for x in processes]
+        
+        # imagedata1 = q1.get()
+        # imagedata2 = q2.get()
+        
+        """ctx = mp.get_context('spawn')
+        
+        q1 = ctx.Queue()
+        q2 = ctx.Queue()
+            
+        p1 = ctx.Process(target=self.run_check, args=(imagedata, 0, 0, amountx, amounty//2, q1))
+        p2 = ctx.Process(target=self.run_check, args=(imagedata, 0, amounty//2, amountx, amounty//2, q2))
+        
+        p1.start()
+        p2.start()
+        imagedata_1 = q1.get()
+        imagedata_2 = q2.get()
+        p1.join()
+        p2.join()"""
+        
+        # imagedata = self.run_check(imagedata, 0, 0, amountx, amounty//2)
+        
+        # imagedata = self.run_check(imagedata, 0, amounty//2, amountx, amounty//2)
+        
+        with fits.open(self.imagepath) as image:
+            image[0].data = imagedata
+            image.writeto("output.fts", overwrite=True)
     
-    def close(self):
-        self.image.close()
+    """def close(self):
+        self.image.close()"""
         
         
     def verifyDateDark(self):
-        if self.image[0].header["DATE"] != self.dark[0].header["DATE"]:
+        if self.imagehdr["DATE"] != self.darkhdr["DATE"]:
             return False
     
     def verifyDateBias(self):
-        if self.image[0].header["DATE"] != self.bias[0].header["DATE"]:
+        if self.imagehdr["DATE"] != self.biashdr["DATE"]:
             return False
         
     def scale_dark(self):
-        if self.dark[0].header["EXPTIME"] != self.image[0].header["EXPTIME"]:
+        if self.darkhdr["EXPTIME"] != self.imagehdr["EXPTIME"]:
             # ...
             pass
     
@@ -112,53 +164,59 @@ class CalibrationPipe():
         return imagedata
         
     def run_check(self, imagedata, startx, starty, amountx, amounty):
+        print("A process was started")
         for y in range(amounty-1):
             for x in range(amountx):
                 try:
                     data = self.remove_hotpixel(x + startx, y + starty, imagedata)
                 except IndexError:
                     pass
-        return data  
-        
-print("start")
+        print(data)
+        # q.put(data)  
 
-image_path = '2021-04-02T19-10-26_M1_Clear_280s_Simon-H.fts'
-b = fits.open('HAT-P-10-001dark.fit')
-c = fits.open('Bias-001.fit')
-d = fits.open('HAT-P-10-001light.fit')
-e = fits.open("Reg_2021-03-31T23-55-09_M51_Red_280s_Benjamin-A.fit")
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
 
-calibPip = CalibrationPipe(image_path, b, c, d)
-calibPip.calibrate()
+    print("start")
 
-"""timeStats = []
-startTime = time.time()
-imgs = [c, c]
-calibPip.stack_images(imgs, "median")
-timeStats.append(time.time() - startTime)
+    image_path = '2021-04-02T19-10-26_M1_Clear_280s_Simon-H.fts'
+    b = 'HAT-P-10-001dark.fit'
+    c = 'Bias-001.fit'
+    d = 'HAT-P-10-001light.fit'
+    e = "Reg_2021-03-31T23-55-09_M51_Red_280s_Benjamin-A.fit"
 
-startTime = time.time()
-imgs = [c, c, c]
-calibPip.stack_images(imgs, "median")
-timeStats.append(time.time() - startTime)
+    calibPip = CalibrationPipe(image_path, b, c, d)
+    # calibPip.close()
+    calibPip.run()
 
-startTime = time.time()
-imgs = [c, c, c, c]
-calibPip.stack_images(imgs, "median")
-timeStats.append(time.time() - startTime)
+    """timeStats = []
+    startTime = time.time()
+    imgs = [c, c]
+    calibPip.stack_images(imgs, "median")
+    timeStats.append(time.time() - startTime)
 
-plt.plot([1,2,3], timeStats)
-plt.show()"""
+    startTime = time.time()
+    imgs = [c, c, c]
+    calibPip.stack_images(imgs, "median")
+    timeStats.append(time.time() - startTime)
 
-now=time.time()
-imgs = [fits.open(image_path), e]
-calibPip.stack_images(imgs, "median")
-calibPip.stack_images(imgs, "average")
+    startTime = time.time()
+    imgs = [c, c, c, c]
+    calibPip.stack_images(imgs, "median")
+    timeStats.append(time.time() - startTime)
+
+    plt.plot([1,2,3], timeStats)
+    plt.show()"""
+
+    """now=time.time()
+    imgs = [fits.open(image_path), e]
+    calibPip.stack_images(imgs, "median")
+    calibPip.stack_images(imgs, "average")"""
 
 
-calibPip.close()
+    # calibPip.close()
 
-print('stashing: ' + str(time.time() - now))
-print('total: ' + str(time.time() - t))
+    # print('stashing: ' + str(time.time() - now))
+    print('total: ' + str(time.time() - t))
 
-print("stop")
+    print("stop")
